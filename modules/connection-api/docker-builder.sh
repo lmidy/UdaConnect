ebcf7e3c6bc9c6a1c8d3ddd2fcc28bfc572a277d
lmidy <lynette.midy@gmail.com>
2022-02-27 19:59:34 -0600

    docker build shell scripts for each api

---
+++ modules/connection-api/docker-builder.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+docker build -t lynettemidy/connection-api:v1.0.0 .
+docker push lynettemidy/connection-api:v1.0.0
---
+++ modules/person-api/docker-builder.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+docker build -t lynettemidy/person-api:v1.0.0 .
+docker push lynettemidy/person-api:v1.0.0

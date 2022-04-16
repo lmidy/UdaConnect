ec1d693d377b66aee623876a291a66ae6ab6987b
lmidy <lynette.midy@gmail.com>
2022-04-16 18:05:27 -0500

    creation of location-consumer

---
+++ modules/location-consumer/docker-builder.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+docker build -t lynettemidy/location-api:v1.0.0 .
+docker push lynettemidy/location-api:v1.0.0
---
+++ modules/location-consumer/main.py
@@ -0,0 +1,19 @@
+import os
+from app.models import Location
+from app.services import LocationService
+from kafka import KafkaConsumer
+
+# Kafka consumer initialization
+TOPIC_NAME = os.environ["TOPIC_NAME"]
+KAFKA_SERVER = os.environ["KAFKA_SERVER"]
+consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=KAFKA_SERVER)
+
+# Location consumer startup
+print("Starting locations consumer...")
+try:
+    for message in consumer:
+        print(message.value.decode('utf-8'))
+        location: Location = LocationService.create(message.value.decode('utf-8'))
+except KeyboardInterrupt:
+    print("Stopping locations consumer...")
+    consumer.close()

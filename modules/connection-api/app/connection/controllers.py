c76ff47d335fcafbeb3b3dec1eb606629a0e49c7
lmidy <lynette.midy@gmail.com>
2022-04-16 18:03:05 -0500

    connection api cleanup

--- modules/connection-api/app/connection/controllers.py
+++ modules/connection-api/app/connection/controllers.py
@@ -17,8 +17,6 @@ DATE_FORMAT = "%Y-%m-%d"
 api = Namespace("Connection", description="Connections via geolocation.")  # noqa
 
 
-# TODO: This needs better exception handlingß
-
 @api.route("/persons/<person_id>/connection")
 @api.param("start_date", "Lower bound of date range", _in="query")
 @api.param("end_date", "Upper bound of date range", _in="query")
--- modules/connection-api/app/routes.py
+++ modules/connection-api/app/routes.py
@@ -1,5 +1,5 @@
-def register_routes(api, app, root="api"):
-    from app.connection import register_routes as attach_connection
+def register_routes(api, app, root="connection-api"):
+    from app.udaconnect import register_routes as attach_udaconnect
 
     # Add routes
-    attach_connection(api, app)
+    attach_udaconnect(api, app)
